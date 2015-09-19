c9d4298ecf330f43f569e72dc4fe7d45e2f81cb0
Kazantsev Alexey <kazantsevalexeyukraine@gmail.com>
2015-09-19 13:43:38 +0300

    Create cell in ControllerArticles

---
+++ feedbackme/controllers/view_articles/ViewCellWithImage.m
@@ -0,0 +1,23 @@
+//
+//  ViewCellWithImage.m
+//  feedbackme
+//
+//  Created by Казанцев Алексей on 9/19/15.
+//  Copyright (c) 2015 Казанцев Алексей. All rights reserved.
+//
+
+#import "ViewCellWithImage.h"
+
+@implementation ViewCellWithImage
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
