26341fc00c444284d442fb278f117c87b113966f
Kazantsev Alexey <kazantsevalexeyukraine@gmail.com>
2015-09-19 13:13:01 +0300

    Add ControllerViewArticles

---
+++ feedbackme/ControllerViewArticles.h
@@ -0,0 +1,13 @@
+//
+//  ControllerViewArticles.h
+//  feedbackme
+//
+//  Created by Казанцев Алексей on 9/19/15.
+//  Copyright (c) 2015 Казанцев Алексей. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ControllerViewArticles : UIViewController
+
+@end
